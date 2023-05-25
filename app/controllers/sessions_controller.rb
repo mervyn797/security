a76788a4b2147332cfeb9b2f280b2ac5e595af5a
Mervyn Pereira <mervyn.pereira@kellogg.northwestern.edu>
2023-05-25 02:27:57 +0000

    sync1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,24 @@ class SessionsController < ApplicationController
   def create
     # authenticate the user
     # 1. try to find the user by their unique identifier
+    @user = User.find_by({"email" => params["email"]})
+    if @user
+      password_the_user_typed= params["password"]
+      password_in_the_database = @user["password"]
+        if BCrypt::Password.new(password_in_the_database) == password_the_user_typed
+          flash["notice"] = "Success!"
+          redirect_to "/companies"
+        else
+          flash["notice"] = "nope."
+          redirect_to "/sessions/new"
+        end
     # 2. if the user exists -> check if they know their password
     # 3. if they know their password -> login is successful
     # 4. if the email does not exist or they do not know their password -> login fails
-    flash["notice"] = "Nope."
-    redirect_to "/sessions/new"
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
